375f0f19db7031085500fa476cc1be7d9fa81ae2
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-08-08 10:15:36 +0300

    Feat: Started autocomplete search

---
+++ client/src/components/__test__/autocomplete-search.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  act,
+  render,
+  wait,
+  fireEvent,
+  screen
+} from "@testing-library/react";
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import { ExpansionPanelActions } from '@material-ui/core';
+
+// Component being tested
+import SearchLocationInput from './../autocomplete-search'
+
+describe("autocomplete-search", () => {
+  it("Should autosearch based on partial input", () => {
+    render(<SearchLocationInput />)
+    
+  });
+})
