033be768da7327699c985a5939a2722a757df1ea
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-08-11 14:53:52 +0300

    FEAT: Email Me Modal active and functional

--- client/src/views/MapItinerary.jsx
+++ client/src/views/MapItinerary.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Map from '../components/map';
 import ItineraryList from '../containers/itinerary-list';
 import Grid from '@material-ui/core/Grid';
 import TopBar from '../components/top-bar';
 import Modal from 'react-modal';
 
+// REMOVE
 import moment from 'moment';
 const startDate = moment().format();
 const endDate = moment().format();
 const mocks = require('./../utils/__test__/mocks');
 const places = mocks.mocks.places;
+//
 
 // function MapItinerary({ places, removePlace, tripDuration, handleAsssignDay, startDate, endDate }) {
 function MapItinerary({ removePlace, tripDuration, handleAsssignDay}) { 
 
   let uniquePlaces = new Set(Object.values(places));
 
+  const [ isModalOpen, setIsModalOpen ] = useState(false);
+
+  const handleEmailMe = function () {
+    setIsModalOpen(true);
+  }
+
+  const handleCloseModal = function () {
+    setIsModalOpen(false);
+  }
+
   return (
     <div>
-      <TopBar heading="Map" buttonPath="/MapItinerary" buttonName="Email Me" />
+      <TopBar heading="Map" action={handleEmailMe} buttonName="Email Me" />
       <Grid container direction="row">
         <Grid item xs={6}>
           <div style={{ position: 'fixed' }}>
@@ -38,8 +50,12 @@ function MapItinerary({ removePlace, tripDuration, handleAsssignDay}) {
           </div>
         </Grid>
       </Grid>
-      <Modal>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseModal}
+      >
         This is a test
+        <button onClick={handleCloseModal}>Close modal</button>
       </Modal>
     </div>
   );
