0ea98930278b8ac3b12bc724f6abe586416ffd80
Rushabh Mehta <rushabh.r.mehta@gmail.com>
2020-08-11 19:49:04 +0300

    FEAT: email controller functional but not configured

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer')
+
 const port = 3079;
 const router = require('./router');
 const cors = require('cors');
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded(
+  {extended: true}
+))
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')))
 app.use(router);
 app.listen(port, () => {
   console.log(`server is listening at http://localhost:${port} 🚀🚀🚀`); // eslint-disable-line no-console
